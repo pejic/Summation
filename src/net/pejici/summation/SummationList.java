59a3d5d700875afdff2b8b49e0de07efd224a4a4
Slobodan Pejic <slobo@pejici.net>
2013-11-04 22:19:58 -0700

    Code cleanup.

--- src/net/pejici/summation/SummationList.java
+++ src/net/pejici/summation/SummationList.java
@@ -5,11 +5,8 @@ import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
